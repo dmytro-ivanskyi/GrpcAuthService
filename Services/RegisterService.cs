28a817a5de2f81ff483ea264bb06d5f47744376d
Dmytro Ivanskyi <ivanskoy.d@gmail.com>
2020-08-14 15:05:23 +0300

    Add EF

---
+++ Services/RegisterService.cs
@@ -0,0 +1,39 @@
+﻿using Data.Models;
+using Data.RepoInterfaces;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+namespace GrpcAuthService.Services
+{
+    public class RegisterService: Register.RegisterBase
+    {
+        private readonly IUserRepo _repo;
+        public RegisterService(IUserRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public override async Task<UserReply> RegisterUser(UserRequest request, ServerCallContext context)
+        {
+            var user = new User
+            {
+                Name = request.Name,
+                Email = request.Email
+            };
+            var created = await _repo.CreateUserAsync(user);
+            if (!created)
+                return null;
+
+            var reply = await _repo.GetUserByIdAsync(user.Id);
+            return new UserReply { Id = reply.Id, Name = reply.Name, Email = reply.Email };
+        }
+
+        //        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        //        {
+        //            return Task.FromResult(new HelloReply
+        //            {
+        //                Message = "Hello " + request.Name
+        //            });
+        //        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data;
+using Data.RepoInterfaces;
+using Data.Repositories;
+using GrpcAuthService.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,11 +18,25 @@ namespace GrpcAuthService
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IUserRepo, UserRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +51,7 @@ namespace GrpcAuthService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<RegisterService>();
 
                 endpoints.MapGet("/", async context =>
                 {
